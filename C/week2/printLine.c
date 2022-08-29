a037a17157f4cecc8df74681c5ea69749080e8da
Manveen Kaur <mkaur39@calstatela.edu>
2022-08-29 17:52:17 +0000

    First commit

---
+++ C/week2/printLine.c
@@ -0,0 +1,16 @@
+/*A simple C program called printLine.c
+ * printLine.c prints a line of text
+ * Written by: Manveen Kaur on 08/29/2022
+ * Using examples in "C How to Program, Harvey and Paul Deitel"
+ */
+
+#include<stdio.h>
+
+/*the function 'main' begins program execution*/
+
+int main( void )
+{
+    printf("This is our first C program\n");
+
+    return 0; /*indicates that the program returns succesfully*/
+} /*end main function*/
