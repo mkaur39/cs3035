0490398a81b9920107c1e02c46aa5808d156c273
mkaur39 <mkaur39@calstatela.edu>
2022-11-01 21:46:33 +0000

    Week 11 Haskell

---
+++ haskell/week11/curriedFuncExample.hs
@@ -0,0 +1,5 @@
+add :: (Num a) => a -> a -> a
+add a b = a + b
+
+main = do
+    print (add 4 5)
---
+++ haskell/week11/partillyAppliedFuncExample.hs
@@ -0,0 +1,4 @@
+addTen :: (Floating a) => a -> a
+addTen = (+10)
+
+--load program into the interctive shell to run
